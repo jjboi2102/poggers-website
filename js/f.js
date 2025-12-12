0da622528efc7ecfeb1b9f2b149400a4af298587
JJ boi <118691619+jjboi2102@users.noreply.github.com>
2025-12-12 15:14:02 -0600

    Create f.js
    thanks jeffalo

---
+++ js/f.js
@@ -0,0 +1,2 @@
+javascript:(function(){['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){document.body.style[prefix + 'transform'] = 'rotate(180deg)';});}())
+// i stole this from jeffalo :)
