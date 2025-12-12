c5467b60b994a8003a5cb76d72af264280b20031
JJ boi <118691619+jjboi2102@users.noreply.github.com>
2025-12-12 15:22:03 -0600

    Update f.js

--- js/f.js
+++ js/f.js
@@ -1,2 +1,5 @@
-javascript:(function(){['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){document.body.style[prefix + 'transform'] = 'rotate(180deg)';});}())
 // i stole this from jeffalo :)
+['', '-ms-', '-webkit-', '-o-', '-moz-'].forEach(prefix => {
+  document.body.style[prefix + 'transform'] = 'rotate(180deg)';
+});
+export {};
