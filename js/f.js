7aea94afb15798297f1e036d7a7427aa6ef8cb2c
JJ boi <118691619+jjboi2102@users.noreply.github.com>
2026-01-16 01:52:22 -0600

    Update f.js

--- js/f.js
+++ js/f.js
@@ -1,4 +1,5 @@
 // i stole this from jeffalo :)
+// javascript:(function(){['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix){document.body.style[prefix + 'transform'] = 'rotate(180deg)';});}())
 ['', '-ms-', '-webkit-', '-o-', '-moz-'].forEach(prefix => {
   document.body.style[prefix + 'transform'] = 'rotate(180deg)';
 });
